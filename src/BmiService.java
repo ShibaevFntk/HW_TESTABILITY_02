b84bccccf1ea9032b9f8a2a0d8350910a1e91b0a
ShibaevFntk <ShibaevFntk@gmail.com>
2025-05-22 14:38:14 +0300

    HW update

--- src/BmiService.java
+++ src/BmiService.java
@@ -2,7 +2,7 @@ public class BmiService {//создал класс
     public double calculate(int m, int h) {//Определил метод, задал параметр
 
         //ИМТ = масса тела (кг) / рост² (м²)
-        return Math.round((double)m/((double)h*(double)h/10000) * 100.0) / 100.0; //m / ((int)hh*(int)hh);
+        return Math.round((double)m/((double)h*(double)h/10000) * 100.0) / 100.0;
 
     }
 }
